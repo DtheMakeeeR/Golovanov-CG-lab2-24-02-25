b551834ae9c2afe24a74306e7b6901c3e6e4b712
DtheMakeeeR <danilgol45@mail.ru>
2025-03-10 19:28:58 +0300

    Update CrossFilter.cs

--- Golovanov-CG-lab2-24-02-25/CrossFilter.cs
+++ Golovanov-CG-lab2-24-02-25/CrossFilter.cs
@@ -22,13 +22,8 @@ namespace Golovanov_CG_lab2_24_02_25
         }
         public override Color CalculateNewPixelColor(Bitmap image, int x, int y)
         {
-            float otnoshenie = image.Width / image.Height;
-            int c = 0;
-            if (x == 100 && y == 50)
-            {
-                c++;
-            }
-            if ((x/ otnoshenie > y) && (x / (otnoshenie) > image.Width / otnoshenie - y))
+            float otnoshenie = (float)image.Width / (float)image.Height;
+            if ((x / otnoshenie > y) && (x / (otnoshenie) > image.Width / otnoshenie - y))
                 return glassFilter.CalculateNewPixelColor(image, x, y);
             if ((x / otnoshenie < y) && (x / (otnoshenie) > image.Width / otnoshenie - y))
                 return sepiaFilter.CalculateNewPixelColor(image, x, y);
